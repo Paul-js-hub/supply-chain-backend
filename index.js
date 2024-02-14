8ba6a3380fdb07c7ef237d43f356bb4fc4a83e9c
Paul-js-hub <hesponpoisson@gmail.com>
2024-02-14 15:38:38 +0300

    Set up the project
    Add api endpoints

---
+++ index.js
@@ -0,0 +1,60 @@
+import express from "express";
+import cors from "cors";
+import { PrismaClient } from "@prisma/client";
+
+const app = express();
+const prisma = new PrismaClient();
+
+const port = process.env.PORT || 8080;
+
+app.use(cors())
+app.use(express.json())
+app.get("/", (req, res) =>{
+  res.json({message: "Alive"})
+});
+
+app.get("/categories", async(req, res) => {
+  const allCategories = await prisma.category.findMany({
+    include: {
+      products: true
+    }
+  })
+  res.status(200).json(allCategories)
+});
+
+app.get("/products", async(req, res) => {
+  const allProducts = await prisma.product.findMany()
+  res.status(200).json(allProducts)
+});
+
+app.post("/categories", async(req, res) =>{
+  const {name, description} = req.body
+  const result = await prisma.category.create({
+    data: {
+      name,
+      description
+    }
+  })
+
+  res.status(201).json(result)
+})
+
+app.post("/products", async(req, res) =>{
+  const {name, price, description, categoryID} = req.body
+  const result = await prisma.product.create({
+    data: {
+      name,
+      price,
+      description,
+      categoryID
+    }
+  })
+
+  res.status(201).json(result)
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Listening to requests on port ${port}`);
+})
---
+++ prisma/migrations/20240213154614_add_categories_products_table/migration.sql
@@ -0,0 +1,22 @@
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "description" TEXT,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "price" DECIMAL(9,2) NOT NULL,
+    "description" TEXT,
+    "categoryID" INTEGER NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_categoryID_fkey" FOREIGN KEY ("categoryID") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
