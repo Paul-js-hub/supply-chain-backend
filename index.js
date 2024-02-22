a819856a34b960940d2694ad6613c4847aa824ca
Paul-js-hub <hesponpoisson@gmail.com>
2024-02-22 17:45:51 +0300

    Reorder categories in asc order after updating them in the database

--- index.js
+++ index.js
@@ -7,67 +7,98 @@ const prisma = new PrismaClient();
 
 const port = process.env.PORT || 8080;
 
-app.use(cors())
-app.use(express.json())
-app.get("/", (req, res) =>{
-  res.send('root')
+app.use(cors());
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("root");
 });
 
 //Categories
-app.get("/categories", async(req, res) => {
-  const allCategories = await prisma.category.findMany({
-    include: {
-      products: true
-    }
-  })
-  res.status(200).json(allCategories)
+app.get("/categories", async (req, res) => {
+  try {
+    const allCategories = await prisma.category.findMany({
+      orderBy: {
+        id: "asc",
+      },
+      include: {
+        products: true,
+      },
+    });
+    res.status(200).json(allCategories);
+  } catch (error) {
+    console.error("Error deleting parent and children:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
-app.post("/categories", async(req, res) =>{
-  const {name, description} = req.body
+app.post("/categories", async (req, res) => {
+  const { name, description } = req.body;
   const result = await prisma.category.create({
     data: {
       name,
-      description
-    }
-  })
+      description,
+    },
+  });
+  if (!result) {
+    res.status(500).json({ error: "Internal Server Error" });
+  } else {
+    res.status(201).json({ result, message: `Category added successfully` });
+  }
+});
 
-  res.status(201).json(result)
-})
-
-app.get("/categories/:id", async(req, res) =>{
-  const {id} = req.params
+app.get("/categories/:id", async (req, res) => {
+  const { id } = req.params;
   const getOneCategory = await prisma.category.findUnique({
     where: {
-      id: Number(id)
-    }
-  })
-  if(!getOneCategory){
-    res.status(200).json({message: `The Category with that particular ID doesn't exist`})
+      id: Number(id),
+    },
+  });
+  if (!getOneCategory) {
+    res
+      .status(200)
+      .json({ message: `The Category with that particular ID doesn't exist` });
   } else {
-    res.status(200).json(getOneCategory)
+    res.status(200).json(getOneCategory);
   }
-})
-app.put("/categories/:id", async(req, res) =>{
-  const {id} = req.params;
-  const {name, description} = req.body
-  const updatedCategory = await prisma.category.update({
-    where: {id: Number(id)},
-    data: {
-      name,
-      description
-    }
-  })
-
-  if(!updatedCategory){
-    res.status(404).json({message: `The Category with that particular ID doesn't exist`})
+});
+app.put("/categories/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+  const { name, description } = req.body;
+  try {
+    await prisma.category.update({
+      where: { id: id },
+      data: {
+        name,
+        description,
+      },
+    });
+    const remainingCategories = await prisma.category.findMany({
+      orderBy: {
+        id: "asc",
+      },
+    });
+    res.status(200).json({
+      remainingCategories,
+      message: `The Category updated successfully`,
+    });
+  } catch (error) {
+    console.error("Error deleting parent and children:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
-  res.status(200).json({updatedCategory, message: `The Category updated successfully`})
-})
 
-app.delete("/categories/:id", async(req, res) =>{
-  const id = parseInt(req.params.id)
-  try{
+  // if (!updatedCategory) {
+  //   res
+  //     .status(404)
+  //     .json({ message: `The Category with that particular ID doesn't exist` });
+  // }
+  // res
+  //   .status(200)
+  //   .json({ updatedCategory, message: `The Category updated successfully` });
+});
+
+app.delete("/categories/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+  try {
     // Begin transaction
     await prisma.$transaction([
       // Delete parent record
@@ -79,57 +110,60 @@ app.delete("/categories/:id", async(req, res) =>{
         where: { id },
       }),
     ]);
-    res.status(200).json({message: `Category with ID ${id} deleted successfully`})
-  } catch (error){
-    console.error('Error deleting parent and children:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    const remainingCategories = await prisma.category.findMany({
+      orderBy: {
+        id: "asc",
+      },
+    });
+    res
+      .status(200)
+      .json({
+        remainingCategories,
+        message: `Category with ID ${id} deleted successfully`,
+      });
+  } catch (error) {
+    console.error("Error deleting parent and children:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
-  // const deletedCategory = await prisma.category.delete({
-  //   where: {
-  //     id: Number(id)
-  //   }
-  // })
-  // if(!deletedCategory){
-  //   res.status(404).json({message: `Category with ID ${id} doesn't exist`})
-  // } else {
-  //   res.status(200).json({deletedCategory, message: `Category with ID ${id} deleted successfully`})
-  // }
-})
-
-//Products
-app.get("/products", async(req, res) => {
-  const allProducts = await prisma.product.findMany()
-  res.status(200).json(allProducts)
 });
 
-app.post("/products", async(req, res) =>{
-  const {name, price, description, categoryID} = req.body
+//Products
+app.get("/products", async (req, res) => {
+  const allProducts = await prisma.product.findMany();
+  res.status(200).json(allProducts);
+});
+
+app.post("/products", async (req, res) => {
+  const { name, price, description, categoryID } = req.body;
   const result = await prisma.product.create({
     data: {
       name,
       price,
       description,
-      categoryID: parseInt(categoryID)
-    }
-  })
+      categoryID: parseInt(categoryID),
+    },
+  });
 
-  res.status(201).json(result)
-})
+  res.status(201).json({ result, message: `Category added Successfully` });
+});
 
-app.delete("/products/:id", async(req, res) =>{
-  const {id} = req.params
+app.delete("/products/:id", async (req, res) => {
+  const { id } = req.params;
   const deletedProduct = await prisma.product.delete({
     where: {
-      id: Number(id)
-    }
-  })
-  if(!deletedProduct){
-    res.status(404).json({message: `Product with ID ${id} doesn't exist`})
+      id: Number(id),
+    },
+  });
+  if (!deletedProduct) {
+    res.status(404).json({ message: `Product with ID ${id} doesn't exist` });
   } else {
-    res.status(200).json({deletedProduct, message: `Product with ID ${id} deleted successfully`})
+    res.status(200).json({
+      deletedProduct,
+      message: `Product with ID ${id} deleted successfully`,
+    });
   }
-})
+});
 
 app.listen(port, () => {
   console.log(`Listening to requests on port ${port}`);
-})
+});
--- prisma/migrations/20240222125055_add_created_at/migration.sql
+++ prisma/migrations/20240222125055_add_created_at/migration.sql
@@ -3,6 +3,7 @@ CREATE TABLE "Category" (
     "id" SERIAL NOT NULL,
     "name" VARCHAR(255) NOT NULL,
     "description" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
 );
@@ -13,10 +14,11 @@ CREATE TABLE "Product" (
     "name" VARCHAR(255) NOT NULL,
     "price" DECIMAL(9,2) NOT NULL,
     "description" TEXT,
-    "categoryID" INTEGER,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "categoryID" INTEGER NOT NULL,
 
     CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
 );
 
 -- AddForeignKey
-ALTER TABLE "Product" ADD CONSTRAINT "Product_categoryID_fkey" FOREIGN KEY ("categoryID") REFERENCES "Category"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "Product" ADD CONSTRAINT "Product_categoryID_fkey" FOREIGN KEY ("categoryID") REFERENCES "Category"("id") ON DELETE CASCADE ON UPDATE CASCADE;
